a79c61f0ba0479bc8f48bb0cc743dab38d3a9c4d
Guilherme Bonfim <6onf1mtech@gmail.com>
2025-02-04 19:48:34 -0300

    Adicionando CadastroDeVeiculos

--- src/CadastroDeVeiculos/Veiculo.java
+++ src/CadastroDeVeiculos/Veiculo.java
@@ -1,10 +1,35 @@
 package CadastroDeVeiculos;
 
-public abstract class Veiculo {
-    String modelo;
-    String placa;
-    double valorDiaria;
-    boolean disponivel;
+public class Veiculo {
+    private String modelo;
+    private String placa;
+    private double valorDiaria;
+    private boolean disponivel;
 
+    public Veiculo(String modelo, String placa, double valorDiaria, boolean disponivel) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.valorDiaria = valorDiaria;
+        this.disponivel = disponivel;
+    }
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
