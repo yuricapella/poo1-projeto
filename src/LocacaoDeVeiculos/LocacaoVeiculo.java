23b3c29e56f81d05332ad63b5aa94b6c53d5d919
Jeverson Santos <jeverson.apsantos@gmail.com>
2025-02-03 21:37:07 -0300

    teste de pull request

--- src/LocacaoDeVeiculos/LocacaoVeiculo.java
+++ src/LocacaoDeVeiculos/LocacaoVeiculo.java
@@ -3,12 +3,14 @@ package LocacaoDeVeiculos;
 public class LocacaoVeiculo {
     public int periodoLocacao;
 
-    double calcularValorTotal(){
+    double calcularValorTotal(double valorLocacao, double periodo) {
 
+        return periodo * valorLocacao;
     }
 
     boolean verificaVeiculoDisponivel(){
 
+    return false;
     }
 
 
